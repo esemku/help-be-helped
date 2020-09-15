3b3dd78bf0cecb8a6d9e812882fabc4f155e4e05
Tomasz Stefaniuk <t.stefaniuk@icloud.com>
2020-09-15 18:16:16 +0200

    Backend corrections

--- backend/models/auth.model.js
+++ backend/models/auth.model.js
@@ -1,70 +1,72 @@
 const mongoose = require('mongoose')
 const crypto = require('crypto')
-
-// User Schema
-const userSchema = new mongoose.Schema({
-  email: {
-    type: String,
-    trim: true,
-    required: true,
-    unique: true,
-    lowercase: true
+// user schema
+const userScheama = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      trim: true,
+      required: true,
+      unique: true,
+      lowercase: true
+    },
+    name: {
+      type: String,
+      trim: true,
+      required: true
+    },
+    hashed_password: {
+      type: String,
+      required: true
+    },
+    salt: String,
+    role: {
+      type: String,
+      default: 'subscriber'
+    },
+    resetPasswordLink: {
+      data: String,
+      default: ''
+    }
   },
-  name: {
-    type: String,
-    trim: true,
-    required: true
-  },
-  hashed_password: {
-    type: String,
-    required: true
-  },
-  salt: String,
-  role: {
-    type: String,
-    default: 'Normal'
-  },
-  resetPasswordLink: {
-    data: String,
-    default: ''
+  {
+    timestamps: true
   }
-}, {timeStamp: true})
+)
 
-
-// Virtual Password
-userSchema.virtual('password')
-  .set(function(password){
-    this.password = password
+// virtual
+userScheama
+  .virtual('password')
+  .set(function(password) {
+    this._password = password
     this.salt = this.makeSalt()
     this.hashed_password = this.encryptPassword(password)
   })
-  .get(function(){
+  .get(function() {
     return this._password
   })
 
-  // methods
-  userSchema.methods = {
-    // Generate Salt
-    makeSalt: function(){
-      return Math.round(new Date().valueOf()*Math.random()) + ''
-    },
-    // Encrypt Password
-    encryptPassword: function(password){
-      if(!password) return ''
-      try{
-        return crypto
-          .createHmac('sha1', this.salt)
-          .update(password)
-          .digest('hex')
-      } catch(err){
-        return ''
-      }
-    },
-    // Compare password between plain get from user and hashed
-    authenticate: function(planPassword){
-      return this.encryptPassword(planPassword) === this.hashed_password
+// methods
+userScheama.methods = {
+  authenticate: function(plainText) {
+    return this.encryptPassword(plainText) === this.hashed_password
+  },
+
+  encryptPassword: function(password) {
+    if (!password) return ''
+    try {
+      return crypto
+        .createHmac('sha1', this.salt)
+        .update(password)
+        .digest('hex')
+    } catch (err) {
+      return ''
     }
+  },
+
+  makeSalt: function() {
+    return Math.round(new Date().valueOf() * Math.random()) + ''
   }
+}
 
-
-  module.exports = mongoose.model('User', userSchema)
+module.exports = mongoose.model('User', userScheama)
